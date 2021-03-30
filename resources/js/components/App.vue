dd9ad7bfc126207541024922207bac3742c907dd
easyscripter <deniscs49@gmail.com>
2021-03-30 19:28:38 +0300

    init project

---
+++ resources/js/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main">
+        <CButton
+                color="success"
+                class="m-2"
+            >
+                Text
+        </CButton>
+  </div>
+</template>
+
+<script>
+import { CSwitch, CButton } from '@coreui/vue';
+export default {
+    components: {
+        CButton
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
