e65e338a0e56c2cdad235cf9d2ac413b6857521f
easyscripter <deniscs49@gmail.com>
2021-04-26 16:43:50 +0300

    feat(front): add google auth with hello js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,27 +2,43 @@
   <div id="app">
     <CSidebar colorScheme="Dark">
       <CSidebarNav>
-        <CSidebarNavTitle><a class="nav-title" href="/">Areal Google Drive</a></CSidebarNavTitle>
+        <CSidebarNavTitle
+          ><a class="nav-title" href="/"
+            >Areal Google Drive</a
+          ></CSidebarNavTitle
+        >
         <CSidebarNavDropdown icon="cil-storage" name="Диски">
-            <CSidebarNavItem to="/my-drive/folder/root" name="Мой диск"></CSidebarNavItem>
-            <CSidebarNavItem to="/common-drives" name="Общие диски"></CSidebarNavItem>
+          <CSidebarNavItem
+            to="/my-drive/folder/root"
+            name="Мой диск"
+          ></CSidebarNavItem>
+          <CSidebarNavItem
+            to="/common-drives"
+            name="Общие диски"
+          ></CSidebarNavItem>
         </CSidebarNavDropdown>
+        <CSidebarNavItem
+          to="/export"
+          icon="cil-cloud-download"
+          name="Экспорт"
+        ></CSidebarNavItem>
         <CSidebarNavItem icon="cil-code" name="Логи"></CSidebarNavItem>
-        <CSidebarNavItem icon="cil-settings" name="Настройки"></CSidebarNavItem>
-        <CSidebarNavItem href="/login/google"
+   <!--     <CSidebarNavItem
+          @click="auth('google')"
           icon="cib-google"
           name="Войти в Google"
           v-if="!user_data.hasOwnProperty('name')"
-        ></CSidebarNavItem  >
-        <CSidebarNavDropdown v-else icon="cil-smile" name="Аккаунт">
-          <CSidebarNavItem>
+        ></CSidebarNavItem>-->
+        <CButton color="primary" @click="auth('google')">Войти в Google</CButton>
+    <!--<CSidebarNavDropdown v-else icon="cil-user" name="Аккаунт">
+         <CSidebarNavItem>
             <a class="c-sidebar-nav-link">
-              <img :src="user_data.avatar" alt="Аватар" srcset="">
-              <p>Привет, {{user_data.name}}</p>
+              <img :src="user_data.avatar" alt="Аватар" srcset="" />
+              <p>Привет, {{ user_data.name }}</p>
             </a>
           </CSidebarNavItem>
           <CSidebarNavItem href="/user/logout" name="Выход"></CSidebarNavItem>
-        </CSidebarNavDropdown>
+        </CSidebarNavDropdown>-->
       </CSidebarNav>
     </CSidebar>
     <CWrapper>
@@ -32,7 +48,6 @@
 </template>
 
 <script>
-import axios from "axios";
 export default {
   data() {
     return {
@@ -40,25 +55,47 @@ export default {
     };
   },
   mounted() {
-    axios
-      .get("https://areal-gdrive.com/api/v1/user")
-      .then(
-        (response) =>
-          (this.user_data = {
-            name: response.data.name,
-            email: response.data.email,
-            avatar: response.data.avatar,
-          })
-      );
+    this.$http.get("https://areal-gdrive.com/api/v1/user").then(
+      (response) =>
+        (this.user_data = {
+          name: response.data.name,
+          email: response.data.email,
+          avatar: response.data.avatar,
+        })
+    );
+  },
+  methods: {
+    auth(network) {
+      const hello = this.hello;
+      hello(network)
+        .login()
+        .then(
+          () => {
+            const authRes = hello(network).getAuthResponse();
+            this.$http
+              .get("api/v1/google-login", {
+                params: {
+                  access_token: authRes.access_token,
+                  provider: network,
+                },
+              })
+              .then((response) => console.log(response.data))
+              .catch((error) => console.log(error));
+          },
+          (e) => {
+            console.log(e);
+          }
+        );
+    },
   },
 };
 </script>
 <style lang="scss">
 @import "~@coreui/coreui/scss/coreui";
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
 
 body {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 }
 .nav-title {
   color: #ffffff;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,42 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import CoreuiVue from '@coreui/vue';
-
-import { CWrapper, CSidebar, CSidebarNavItem,CSidebarNavTitle, CSidebarNav, CSidebarNavDropdown, CCard, CCardBody, CDataTable, CBreadcrumbRouter, CIcon, CSidebarNavLInk } from "@coreui/vue";
-import { cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilSmile} from '@coreui/icons'
+import Axios from 'axios';
+import { cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilUser, cilCloudDownload} from '@coreui/icons'
 import dotenv from 'dotenv'
+
 const HelloJs = require('hellojs/dist/hello.all.min.js');
 const VueHello = require('vue-hellojs');
 
-
-Vue.config.productionTip = false
-
-Vue.use(CoreuiVue);
-
-Vue.component('CWrapper', CWrapper)
-Vue.component('CCard', CCard)
-Vue.component('CSidebar', CSidebar)
-Vue.component('CSidebarNavItem', CSidebarNavItem)
-Vue.component('CSidebarNavTitle', CSidebarNavTitle)
-Vue.component('CSidebarNav', CSidebarNav)
-Vue.component('CSidebarNavDropdown', CSidebarNavDropdown)
-Vue.component('CSidebarNavLInk', CSidebarNavLInk)
-Vue.component('CIcon', CIcon)
-Vue.component('CCardBody', CCardBody)
-Vue.component('CDataTable', CDataTable)
-Vue.component('CBreadcrumbRouter', CBreadcrumbRouter)
-
-dotenv.config()
-
 HelloJs.init({
-  google: '744887238689-i3p531fodl1jjeknp61h738pq9juqbpk.apps.googleusercontent.com'
+  google: '744887238689-i3p531fodl1jjeknp61h738pq9juqbpk.apps.googleusercontent.com',
 }, {
   redirect_uri: '/login/google/callback'
 });
 Vue.use(VueHello, HelloJs);
 
+Vue.prototype.$http = Axios;
+Vue.config.productionTip = false
+
+Vue.use(CoreuiVue);
+
+dotenv.config()
+
 new Vue({
   router,
-  icons: {cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilSmile},
+  icons: {cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilUser, cilCloudDownload},
   render: h => h(App)
 }).$mount('#app')
