a0dfaddb0c2cc9106d8e98b5a0c13da90d0574bf
easyscripter <deniscs49@gmail.com>
2021-04-26 16:41:53 +0300

    feat(back): add google auth with api

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -13,43 +13,43 @@ use Laravel\Socialite\Facades\Socialite;
 class LoginController extends Controller
 {
 
-    public function login() {
-       $parameters = ['access_type' => 'offline'];
-       return Socialite::driver('google')->scopes(["https://www.googleapis.com/auth/drive"])->with($parameters)->redirect();
+    public function loginUser(Request $request) {
+       $user = Socialite::with($request->provider)->stateless()->userFromToken($request->access_token);
+       return response()->json($user, 200);
     }
 
-    public function handleProviderGoogleCalllback(Request $request) {
-        $auth_token = Socialite::with('google')->getAccessTokenResponse($request->code);
-        $user = Socialite::driver('google')->userFromToken($auth_token['access_token']);
+    // public function handleProviderGoogleCalllback(Request $request) {
+    //     $auth_token = Socialite::with('google')->getAccessTokenResponse($request->code);
+    //     $user = Socialite::driver('google')->userFromToken($auth_token['access_token']);
 
-        session([
-            "user"=>[
-                "token"=>$auth_token,
-                "info"=>$user
-            ]
-        ]);
+    //     session([
+    //         "user"=>[
+    //             "token"=>$auth_token,
+    //             "info"=>$user
+    //         ]
+    //     ]);
 
-        return redirect(env('FRONTEND_URL'))->with(session('user'));
-    }
+    //     return redirect(env('FRONTEND_URL'))->with(session('user'));
+    // }
 
-    public function getUser(Request $request) {
-        $user_info = $request->session()->get('user.info');
+    // public function getUser(Request $request) {
+    //     $user_info = $request->session()->get('user.info');
 
-        if (!$user_info) {
-            return response()->json(['success'=> false], 400);
-        }
+    //     if (!$user_info) {
+    //         return response()->json(['success'=> false], 400);
+    //     }
 
-        $response = [
-            "name" => $user_info->name,
-            "email" => $user_info->email,
-            "avatar" => $user_info->avatar
-        ];
-        return response()->json($response, 200);
-    }
+    //     $response = [
+    //         "name" => $user_info->name,
+    //         "email" => $user_info->email,
+    //         "avatar" => $user_info->avatar
+    //     ];
+    //     return response()->json($response, 200);
+    // }
 
-    public function logout(Request $request) {
-        $request->session()->pull('user', 'default');
-        return redirect()->back();
-    }
+    // public function logout(Request $request) {
+    //     $request->session()->pull('user', 'default');
+    //     return redirect()->back();
+    // }
 
 }
