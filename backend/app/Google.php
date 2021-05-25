2a3a632134fa0216a8e106f56109a6d03af71766
easyscripter <deniscs49@gmail.com>
2021-05-25 12:55:41 +0300

    feat(backend): add scopes for google drive api

--- backend/app/Google.php
+++ backend/app/Google.php
@@ -1,5 +1,11 @@
 <?php
+
 namespace App;
+
+use Google_Service_Docs;
+use Google_Service_Drive;
+use Google_Service_Sheets;
+use Google_Service_Slides;
 
 class Google
 {
@@ -9,10 +15,15 @@ class Google
         $client->setClientId(env('GOOGLE_CLIENT_ID'));
         $client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
         $client->setRedirectUri('postmessage');
-        $client->setScopes(explode(',', env('GOOGLE_SCOPES')));
+        $client->setScopes(array(
+            "https://www.googleapis.com/auth/documents",
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/presentations"
+        ));
         $client->setApprovalPrompt(env('GOOGLE_APPROVAL_PROMPT'));
         $client->setAccessType(env('GOOGLE_ACCESS_TYPE'));
-
         return $client;
     }
 
