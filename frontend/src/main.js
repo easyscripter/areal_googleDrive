476299b2e18caa8078d24bab849c1b4ebadf194d
easyscripter <deniscs49@gmail.com>
2021-05-25 12:38:33 +0300

    fix(front): edit clientId for vue-ouath2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import router from './router'
 import CoreuiVue from '@coreui/vue';
 import Axios from 'axios';
-import { cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilUser, cilCloudDownload} from '@coreui/icons'
+import { cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilUser, cilCloudDownload, cilSearch} from '@coreui/icons'
 import dotenv from 'dotenv'
 import GAuth from 'vue-google-oauth2'
 
 
 const gauthOption = {
-  clientId: '744887238689-i3p531fodl1jjeknp61h738pq9juqbpk.apps.googleusercontent.com',
-  scope: 'profile email',
+  clientId: '699896923042-c9aobk41fmcqea58qfg6uggv002e8aq5.apps.googleusercontent.com',
+  scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/presentations',
   prompt: 'select_account',
 }
 Vue.use(GAuth, gauthOption)
@@ -25,6 +25,6 @@ dotenv.config()
 
 new Vue({
   router,
-  icons: {cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilUser, cilCloudDownload},
+  icons: {cilCode, cilHome, cibGoogle, cilSettings, cilStorage, cilArrowLeft, cilUser, cilCloudDownload, cilSearch},
   render: h => h(App)
 }).$mount('#app')
